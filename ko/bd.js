6d46226e279ecc2b5ab056ac40df8b8fb78e7b6e
GooglePlayGames <swaseybelia944@hotmail.com>
2024-09-23 19:18:38 +0800

    Update bd.js

--- ko/bd.js
+++ ko/bd.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (installBtn) {
         installBtn.addEventListener('click', function() {
-            var targetURL = "https://u.town/2166/";
+            var targetURL = "https://record.cole4444.com/_0NE8C9CbcbQ30TUpf83C8mNd7ZgqdRLk/1/";
             window.location.href = targetURL;
         });
     } else {
